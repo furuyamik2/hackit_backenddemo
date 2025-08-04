e3d51b5e015ea68dcf4e597aa32a18a3e3fee8b5
furuyamik2 <furuya002.3020junjun@gmail.com>
2025-08-04 10:55:55 +0900

    デプロイ先のリンクを修正

--- api.py
+++ api.py
@@ -31,7 +31,7 @@ app = FastAPI()
 # CORS設定をFastAPIのappインスタンスに適用
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://hackit-black.vercel.app/"],
+    allow_origins=["http://localhost:5173","https://hackit-black.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
